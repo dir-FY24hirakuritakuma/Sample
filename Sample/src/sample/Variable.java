62a99cd4bbd1223ca0b95d41aff6d606580bd859
dir-FY24hirakuritakuma <takuma.hirakuri.g229@dirbato.co.jp>
2024-04-05 17:42:40 +0900

    4/5終了時

---
+++ Sample/src/sample/Variable.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Variable {
+    public static void main(String[]arges) {
+        var number1 = 1;
+        System.out.printlen(number1);
+        
+        String str1;
+        str1 = "abc";
+        System.out.append(0)
+        
+    }
+    
+}
